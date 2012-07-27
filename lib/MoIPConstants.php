8e488270c5bd319e63da75faf0abdd75d8c88821
Maury M. Marques <maurymmarques@gmail.com>
2012-07-27 18:43:16 -0300

    added MoIP constants

---
+++ lib/MoIPConstants.php
@@ -0,0 +1,10 @@
+<?php
+class MoIPTransactionStatus {
+	const AUTHORIZED = 1;
+	const INITIATE = 2;
+	const BILLET_PRINTED = 3;
+	const COMPLETED = 4;
+	const CANCELED = 5;
+	const IN_ANALYSIS = 6;
+}
+?>
